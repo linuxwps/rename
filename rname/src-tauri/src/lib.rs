852a9d46fb265527e91e54c042fddb951e853c9a
linuxwps <linuxwps@gmail.com>
2026-05-26 21:31:52 +0800

    fix: unused app variable and import in release build

--- rname/src-tauri/src/lib.rs
+++ rname/src-tauri/src/lib.rs
@@ -1,15 +1,14 @@
-use tauri::Manager;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_os::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
             {
-                app.handle().plugin(
+                use tauri::Manager;
+                _app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
                         .build(),
